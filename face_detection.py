769fd33b55fc40412c71f81ee4e19dff32040783
HritickGokul <gokulysai@gmail.com>
2021-03-24 22:51:04 +0530

    Aligned faces in video

--- face_detection.py
+++ face_detection.py
@@ -15,57 +15,61 @@ face_pose_predictor = dlib.shape_predictor(predictor_model)
 face_aligner = FaceAligner(face_pose_predictor, desiredFaceWidth = 256)
 
 #Capturing video from the webcam
-# capture = cv.VideoCapture(0)
-#
-# while True:
-#     #reading single frame at a time
-#     isTrue, frame = capture.read()
-#
-#     #Converting thr frame to gray image
-#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-#
-#     #It gives the coordinates of the face in the image
-#     faces = face_detector(gray, 1)
-#
-#     #drawing a rectangle in the faces
-#     for i, face in enumerate(faces):
-#         predicted_landmarks = face_pose_predictor(gray, face)
-#         for n in range(0, 68):
-#             x = predicted_landmarks.part(n).x
-#             y = predicted_landmarks.part(n).y
-#             cv.circle(frame, (x, y), 1, (0, 255, 255), 1)
-#     cv.imshow("Detected faces", frame)
-#     if cv.waitKey(20) & 0xFF==ord('d'):
-#         break
-# capture.release()
-# cv.destroyAllWindows()
+capture = cv.VideoCapture(0)
+
+while True:
+    #reading single frame at a time
+    isTrue, frame = capture.read()
+
+    # win = dlib.image_window()
+    # win.set_image(frame)
+
+    #Converting thr frame to gray image
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    #It gives the coordinates of the face in the image
+    faces = face_detector(gray, 1)
+
+    #drawing a rectangle in the faces
+    for i, face in enumerate(faces):
+        predicted_landmarks = face_pose_predictor(gray, face)
+        for n in range(0, 68):
+            x = predicted_landmarks.part(n).x
+            y = predicted_landmarks.part(n).y
+            cv.circle(frame, (x, y), 1, (0, 255, 255), 1)
+        alignedFace = face_aligner.align(frame, gray, face)
+    cv.imshow("Landmarks in the face", frame)
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
 ################################################################################
-image = cv.imread("Pictures/robert.jpg")
-cv.imshow('Original Image', image)
-
-win = dlib.image_window()
-win.set_image(image)
-
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
-#It gives the coordinates of the face in the image
-faces = face_detector(gray, 1)
-
-for i, face in enumerate(faces):
-    #drawing a rectangle in the detected faces
-    win.add_overlay(face)
-
-    #Finding the landmarks
-    predicted_landmarks = face_pose_predictor(gray, face)
-    for n in range(0, 68):
-        x = predicted_landmarks.part(n).x
-        y = predicted_landmarks.part(n).y
-        cv.circle(image, (x, y), 1, (0, 255, 255), 1)
-        
-    #Aligning the face
-    alignedFace = face_aligner.align(image, gray, face)
-cv.imshow("Landmarks in detected face", image)
-cv.imshow("Aligned face", alignedFace)
-cv.waitKey(0)
+# image = cv.imread("Pictures/robert.jpg")
+# cv.imshow('Original Image', image)
+#
+# win = dlib.image_window()
+# win.set_image(image)
+#
+# gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+#
+# #It gives the coordinates of the face in the image
+# faces = face_detector(gray, 1)
+#
+# for i, face in enumerate(faces):
+#     #drawing a rectangle in the detected faces
+#     win.add_overlay(face)
+#
+#     #Finding the landmarks
+#     predicted_landmarks = face_pose_predictor(gray, face)
+#     for n in range(0, 68):
+#         x = predicted_landmarks.part(n).x
+#         y = predicted_landmarks.part(n).y
+#         cv.circle(image, (x, y), 1, (0, 255, 255), 1)
+#
+#     #Aligning the face
+#     alignedFace = face_aligner.align(image, gray, face)
+# cv.imshow("Landmarks in detected face", image)
+# cv.imshow("Aligned face", alignedFace)
+# cv.waitKey(0)
 
 ################################################################################
